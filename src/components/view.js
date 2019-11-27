1e828850c40d05b50f48fbcccba61df93e82e162
MorganeBeCode <morgane.jansen@gmail.com>
2019-11-27 16:55:27 +0100

    ajout materialize

--- src/components/view.js
+++ src/components/view.js
@@ -7,26 +7,50 @@ export default class View extends React.Component {
     }
     render() {
         return (
-            <div id={"timer"}>
-                <div id={"clock"}>
-                    <div id={"time"}>{this.props.displayTime}</div>
-                    <div id={"buttons"}>
-                        <button type={"button"} onClick={this.props.startTimer}>
-                            {"Start"}
-                        </button>
-                        <button
-                            type={"button"}
-                            onClick={this.props.incrementTime}>
-                            {"+"}
-                        </button>
-                        <button
-                            type={"button"}
-                            onClick={this.props.decrementTime}>
-                            {"-"}
-                        </button>
-                        <button type={"button"} onClick={this.props.resetTimer}>
-                            {"Reset"}
-                        </button>
+            <div className={"container"} id={"timer"}>
+                <div className={"row"}>
+                    <div
+                        className={"col l8 offset-l2 m8 offset-m2 s12 center"}
+                        id={"clock"}>
+                        <div className={"row valign-wrapper"}>
+                            <div className={"col l10 m8 s6"} id={"time"}>
+                                {this.props.displayTime}
+                            </div>
+                            <div className={"col l2 m4 s6"} id={"buttons"}>
+                                <button
+                                    className={
+                                        "waves-effect waves-light btn-large"
+                                    }
+                                    type={"button"}
+                                    onClick={this.props.startTimer}>
+                                    {"Start"}
+                                </button>
+                                <button
+                                    className={
+                                        "waves-effect waves-light btn-large"
+                                    }
+                                    type={"button"}
+                                    onClick={this.props.incrementTime}>
+                                    {"+"}
+                                </button>
+                                <button
+                                    className={
+                                        "waves-effect waves-light btn-large"
+                                    }
+                                    type={"button"}
+                                    onClick={this.props.decrementTime}>
+                                    {"-"}
+                                </button>
+                                <button
+                                    className={
+                                        "waves-effect waves-light btn-large"
+                                    }
+                                    type={"button"}
+                                    onClick={this.props.resetTimer}>
+                                    {"Reset"}
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
